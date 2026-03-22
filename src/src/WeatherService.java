c22b600ac73203cf556f4fa2fcd8fee24f9d6990
christienDuran <christienduran@gmail.com>
2026-03-22 12:36:48 -0400

    Added variables and function template for Reminder.java and WeatherService.java

--- src/src/WeatherService.java
+++ src/src/WeatherService.java
@@ -1,3 +1,18 @@
 public class WeatherService {
 
+    double longitude;  // variables for location can be changed
+    double latitude;   // latitude and longitude is good for right now
+
+    double[] forecastData;  // we could have separate arrays for temp whether its sunny or cloudy chance of rain
+
+
+    void getWeather() {
+
+    }
+
+    void getForecastData() {
+
+    }
+
+
 }
