abe15609e7e7cbf64be8519729eb9b12de10b057
Flueont <kylehightower4@gmail.com>
2026-04-06 15:29:09 -0400

    comments on what might be here

--- src/src/Budget.java
+++ src/src/Budget.java
@@ -1,2 +1,9 @@
+// is supposed to allow the User to track their finances.
+// Are we going to allow the user to add their own fields like "Food", "Mortgage", "Groceries", or are we going to add it?
+
+import java.time.LocalDate;
+import java.util.ArrayList; // will need this if we are going to integrate an addexpense method call, unless we want
+                            // a grid to simply pop-up for the user to add their info into
+
 public class Budget {
 }
