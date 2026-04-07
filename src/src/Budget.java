4e4cf82db956c19b2f1d6c70edf2ef2148d5d7c4
Flueont <kylehightower4@gmail.com>
2026-04-07 15:56:40 -0400

    Implementation that will be improved further

--- src/src/Budget.java
+++ src/src/Budget.java
@@ -6,4 +6,33 @@ import java.util.ArrayList; // will need this if we are going to integrate an ad
                             // a grid to simply pop-up for the user to add their info into
 
 public class Budget {
+    private String expenseName;
+    private double amount;
+    private LocalDate date; // allow the user to set a time when an expense needs to be paid
+
 }
+
+    // Constructor
+    public Budget(String expenseName, double amount, LocalDate date) {
+        this.expenseName = expenseName;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    //Methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
